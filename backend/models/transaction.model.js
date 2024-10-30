624e8b75c060e83dcc8fcd01986661fd5acd9101
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2024-10-30 22:44:17 +0300

    Refactor .gitignore and add environment file

---
+++ backend/models/transaction.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
