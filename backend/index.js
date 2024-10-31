0b4e764fd991b2c48c06f64ff91aadaa99498bb2
colinmarklubembe <107845562+colinmarklubembe@users.noreply.github.com>
2024-11-01 00:04:40 +0300

    Refactor connectDB.js and index.js files

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,52 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import { buildContext } from "graphql-passport";
+
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+import connectDB from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+
 dotenv.config();
+configurePassport();
+
 const app = express();
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: "sessions",
+});
+
+store.on("error", (error) => console.log(error));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // this option is required and we set it to false to prevent recreating the session every time a request hits the server (which would be a waste of resources)
+    saveUninitialized: false, // this option is required and we set it to false to prevent saving the session if nothing was modified during the request
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      httpOnly: true, // this option is set to true to prevent client-side JavaScript from reading the cookie
+    },
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -24,13 +60,17 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req }) => buildContext({ req }),
   })
 );
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000`);
---
+++ backend/passport/passport.config.js
@@ -0,0 +1,42 @@
+import passport from "passport";
+import bcryptjs from "bcryptjs";
+
+import User from "../models/user.model.js";
+import { GraphQLLocalStrategy } from "graphql-passport";
+
+export const configurePassport = async () => {
+  passport.serializeUser((user, done) => {
+    console.log("serializeUser", user);
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(async (userId, done) => {
+    console.log("deserializeUser", userId);
+    try {
+      const user = await User.findById(userId);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  passport.use(
+    new GraphQLLocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          throw new Error("Invalid username or password");
+        }
+        const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordValid) {
+          throw new Error("Invalid username or password");
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    })
+  );
+};
